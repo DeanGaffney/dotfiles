4723f41f4e4988d6cb18041eea3f6addb2392899
Dean Gaffney <dean.gaffney@genesys.com>
2025-07-22 16:59:52 +0100

    feat: added table title command

--- wezterm.lua
+++ wezterm.lua
@@ -78,6 +78,21 @@ config.keys = {
       end),
     }),
   },
+  {
+    key = "e",
+    mods = "CMD|SHIFT",
+    action = wezterm.action.PromptInputLine({
+      description = "Enter new name for tab",
+      action = wezterm.action_callback(function(window, pane, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    }),
+  },
 }
 
 -- Finally, return the configuration to wezterm:
